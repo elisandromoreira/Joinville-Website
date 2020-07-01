76b6d7add4e4adfbd843820ab4431f6b7e71d24f
Elisandro Moreira <elisandro.moreira@univille.br>
2020-07-01 15:36:19 -0300

    Add Burger Menu Animation

--- script.js
+++ script.js
@@ -59,6 +59,7 @@ function animateSlides() {
 
 const mouse = document.querySelector('.cursor');
 const mouseTxt = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
 
 function cursor(e) {
   mouse.style.top = e.pageY + 'px';
@@ -83,6 +84,25 @@ function activeCursor(e) {
   }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains('active')) {
+    e.target.classList.add('active');
+    gsap.to('.line1', 0.5, { rotate: '45', y: 4, background: 'black' });
+    gsap.to('.line2', 0.5, { rotate: '-45', y: -4, background: 'black' });
+    gsap.to('#logo', 1, { color: 'rgb(86, 124, 228)' });
+    gsap.to('.nav-bar', 1, { clipPath: 'circle(2500px at 100% -10%)' });
+    document.body.classList.add('hide');
+  } else {
+    e.target.classList.remove('active');
+    gsap.to('.line1', 0.5, { rotate: '0', y: 0, background: 'white' });
+    gsap.to('.line2', 0.5, { rotate: '0', y: 0, background: 'white' });
+    gsap.to('#logo', 1, { color: 'white' });
+    gsap.to('.nav-bar', 1, { clipPath: 'circle(50px at 100% -10%)' });
+    document.body.classList.remove('hide');
+  }
+}
+
+burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
 
