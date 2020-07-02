606c7a77ad9bd20de807d9667d658a9ec17cd20a
Elisandro Moreira <elisandro.moreira@univille.br>
2020-07-02 12:19:34 -0300

    New Page and Animations Added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
+let detailScene;
 
 function animateSlides() {
   //Init Controller
@@ -97,13 +98,107 @@ function navToggle(e) {
     gsap.to('.line1', 0.5, { rotate: '0', y: 0, background: 'white' });
     gsap.to('.line2', 0.5, { rotate: '0', y: 0, background: 'white' });
     gsap.to('#logo', 1, { color: 'white' });
-    gsap.to('.nav-bar', 1, { clipPath: 'circle(50px at 100% -10%)' });
+
     document.body.classList.remove('hide');
   }
+}
+
+//Barba Page Transitions
+const logo = document.querySelector('#logo');
+barba.init({
+  views: [
+    {
+      namespace: 'home',
+      beforeEnter() {
+        animateSlides();
+        logo.href = './index.html';
+      },
+      beforeLeave() {
+        slideScene.destroy();
+        pageScene.destroy();
+        controller.destroy();
+      },
+    },
+    {
+      namespace: 'cidade',
+      beforeEnter() {
+        logo.href = '../index.html';
+        detailAnimation();
+      },
+      beforeLeave() {
+        controller.destroy();
+        detailScene.destroy();
+      },
+    },
+  ],
+  transitions: [
+    {
+      leave({ current, next }) {
+        let done = this.async();
+        //An Animation
+        const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+        tl.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+        tl.fromTo(
+          '.swipe',
+          0.75,
+          { x: '-100%' },
+          { x: '0%', onComplete: done },
+          '-=0.5'
+        );
+      },
+      enter({ current, next }) {
+        let done = this.async();
+        //Scroll to the top
+        window.scrollTo(0, 0);
+        //An Animation
+        const tl = gsap.timeline({ defaults: { ease: 'power2.inOut' } });
+        tl.fromTo(
+          '.swipe',
+          1,
+          { x: '0%' },
+
+          { x: '100%', stagger: 0.2, onComplete: done }
+        );
+        tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          '.nav-header',
+          1,
+          { y: '-100%' },
+          { y: '0%', ease: 'power2.inOut' },
+          '-=1.5'
+        );
+      },
+    },
+  ],
+});
+
+function detailAnimation() {
+  controller = new ScrollMagic.Controller();
+  const slides = document.querySelectorAll('.detail-slide');
+  slides.forEach((slide, index, slides) => {
+    const slideTl = gsap.timeline({ defaults: { duration: 1 } });
+    let nextSlide = slides.length - 1 === index ? 'end' : slides[index + 1];
+    const nextImg = nextSlide.querySelector('img');
+    slideTl.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+    slideTl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 }, '-=1');
+    slideTl.fromTo(nextImg, { x: '50%' }, { x: '0%' });
+    //Scene
+    detailScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: '100%',
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(slideTl)
+      // .addIndicators({
+      //   colorStart: "white",
+      //   colorTrigger: "white",
+      //   name: "detailScene"
+      // })
+      .addTo(controller);
+  });
 }
 
 burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
-
-animateSlides();
